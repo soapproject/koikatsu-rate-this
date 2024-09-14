acdddef93a3e278972b81fd09d186be2a1cc7222
soap <breakhanselmain@gmail.com>
2024-09-14 22:03:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# koikatsu-rate-this
+
+This project is a plugin for Koikatsu that allows users to rate character cards directly from the character creation screen (Maker). The plugin adds a "Rate It" checkbox in the right-sidebar. Once toggled, a panel will appear with configurable rating buttons. Based on the selected rating, the character card will be automatically moved to a designated folder under \Koikatsu\UserData\chara\female\{RatingType}.
+
+Key features:
+
+Customizable Rating Categories: Users can define different rating types in the plugin configuration (e.g., "Like", "Dislike", "MissingMod", "OtherIssue").
+Character Management: Easily sort characters by rating, and move them to the appropriate folders.
+Buffer System: Ratings can be assigned to multiple characters and batch-processed in one go.
+Intuitive UI: The rating panel is user-friendly, designed to blend into the existing Koikatsu Maker UI, and easy to navigate.
+This tool is ideal for users who wish to quickly organize their character cards based on personal preferences or issues found in the cards.
---
+++ main.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * This plugin adds a "Rate It" checkbox in the right-sidebar of the character creation screen (Maker).
+ * When the checkbox is toggled, a panel appears with buttons to rate the currently loaded character card.
+ * Based on the rating selected, the character card will be moved to the appropriate folder:
+ * \Koikatsu\UserData\chara\female\{RatingType}
+ * 
+ * TODO: Path should be configable.
+ */
+
+using BepInEx;
+using BepInEx.Configuration;
+using KKAPI.Maker;
+using KKAPI.Maker.UI.Sidebar;
+using UniRx;
+using UnityEngine;
+
+namespace RatingPlugin
+{
+    [BepInPlugin(GUID, "Rating Plugin", Version)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
+    public class RatingPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "com.soapproject.koikatsu.ratingplugin";
+        public const string Version = "1.0.0";
+        public static ConfigEntry<string> RatingTypesConfig;
+
+        private void Awake()
+        {
+            // bind default config
+            RatingTypesConfig = Config.Bind("General", "RatingTypes", "Like,Dislike,MissingMod,OtherIssue", "List of rating types separated by commas.");
+
+            // regiist event which Draw checkbox
+            MakerAPI.RegisterCustomSubCategories += OnMakerSceneLoaded;
+
+            // init Panel
+            var obj = new GameObject("PanelGUI");
+            PanelGUI.Instance = obj.AddComponent<PanelGUI>();
+            DontDestroyOnLoad(obj);
+        }
+
+        // Draw a checkbox in the right-sidebar
+        private void OnMakerSceneLoaded(object sender, RegisterSubCategoriesEvent args)
+        {
+            var toggle = args.AddSidebarControl(new SidebarToggle("Rate It", false, this));
+            toggle.ValueChanged.Subscribe(value =>
+            {
+                togglePanelGUI(value);
+            });
+        }
+
+        private void togglePanelGUI(bool toggleState)
+        {
+            if (!toggleState)
+                PanelGUI.Hide();
+            else
+            {
+                if (MakerAPI.InsideAndLoaded)
+                    PanelGUI.Show();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            MakerAPI.RegisterCustomSubCategories -= OnMakerSceneLoaded;
+        }
+    }
+}
